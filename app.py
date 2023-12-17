b5b6bfaf322a8e283780637939e897a08f612a44
Jash-tech <jashsuke@gmail.com>
2023-12-17 17:06:18 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion,DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformation,DataTransformationConfig
 import sys
 import os
 
@@ -11,6 +12,9 @@ if __name__=="__main__":
 
     try:
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     except Exception as e:
         raise CustomException(e,sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,6 +4,8 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import pandas as pd
 import pymysql
+import pickle
+import numpy as np
 
 
 from dotenv import load_dotenv
@@ -31,3 +33,16 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex)
+    
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
